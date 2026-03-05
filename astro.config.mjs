01e906f6a424d115a83f5f0fae15ad4a5387f5fd
anatoledodata <ana.toledo.data@gmail.com>
2026-03-05 18:51:09 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://anatoledodata.github.io', //linea agregada
-  adapter: edgeoneAdapter(),
-  output: 'static',
-  integrations: [react()]
+  site: 'https://anatoledodata.github.io',
+  integrations: [react()],
+  output: 'static', // Esto le dice a Astro que genere archivos HTML puros
 });
